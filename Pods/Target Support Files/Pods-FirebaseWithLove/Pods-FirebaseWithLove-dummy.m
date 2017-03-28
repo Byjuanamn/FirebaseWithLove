672f88171440b36e83bc29b22a199557ec5d6514
byjuanamn <juan.antonio.martin@gmail.com>
2017-03-28 23:20:00 +0200

    pods incluidos y UI creada

---
+++ Pods/Target Support Files/Pods-FirebaseWithLove/Pods-FirebaseWithLove-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebaseWithLove : NSObject
+@end
+@implementation PodsDummy_Pods_FirebaseWithLove
+@end
---
+++ Pods/Target Support Files/Pods-FirebaseWithLove/Pods-FirebaseWithLove-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FirebaseWithLoveVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FirebaseWithLoveVersionString[];
+
